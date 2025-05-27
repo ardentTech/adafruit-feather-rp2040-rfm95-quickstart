1979f1380fb40b98ad3a2bdee6951e9f12f98bee
Jonathan D. Baker <jonathan@ardent.tech>
2025-05-27 10:32:59 -0600

    working hal-level rtc wake

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,32 @@
 
 mod bsp;
 
+use core::cell::RefCell;
+use cortex_m::asm::{wfe, wfi};
+use cortex_m::interrupt::Mutex;
 use embassy_executor::Spawner;
-use embassy_rp::bind_interrupts;
+use embassy_rp::{bind_interrupts, interrupt};
+use embassy_rp::gpio::{Level, Output};
+use embassy_rp::pac::i2c::vals::Speed;
 use embassy_rp::peripherals::USB;
-use embassy_rp::rtc::{DateTime, DayOfWeek, Rtc};
+use embassy_rp::rtc::{DateTime, DateTimeFilter, DayOfWeek, Rtc};
 use embassy_rp::usb::Driver;
 use embassy_time::Timer;
 use panic_halt as _;
 
-//use crate::bsp::{AssignedResources, DmaResources, GpioResources, I2cResources, LedResources, LoraResources, RtcResources, SpiResources, UartResources, UsbResources};
+static LED: Mutex<RefCell<Option<Output<'static>>>> = Mutex::new(RefCell::new(None));
+static RTC: Mutex<RefCell<Option<Rtc<embassy_rp::peripherals::RTC>>>> = Mutex::new(RefCell::new(None));
 
-bind_interrupts!(struct Irqs {
-    USBCTRL_IRQ => embassy_rp::usb::InterruptHandler<USB>;
-});
+// TODO can this work with logging?
 
-#[embassy_executor::task]
-pub async fn logging(driver: Driver<'static, USB>) {
-    embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
-}
+// bind_interrupts!(struct Irqs {
+//     USBCTRL_IRQ => embassy_rp::usb::InterruptHandler<USB>;
+// });
+
+// #[embassy_executor::task]
+// pub async fn logging(driver: Driver<'static, USB>) {
+//     embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
+// }
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -28,8 +36,8 @@ async fn main(spawner: Spawner) {
 
     //let board = split_resources!(p);
 
-    let usb_driver = Driver::new(p.USB, Irqs);
-    spawner.must_spawn(logging(usb_driver));
+    // let usb_driver = Driver::new(p.USB, Irqs);
+    // spawner.must_spawn(logging(usb_driver));
 
     let mut rtc = Rtc::new(p.RTC);
     if !rtc.is_running() {
@@ -46,17 +54,34 @@ async fn main(spawner: Spawner) {
         rtc.set_datetime(now).unwrap();
     }
 
-    Timer::after_millis(20000).await;
-
-    if let Ok(dt) = rtc.now() {
-        log::info!(
-            "Now: {}-{:02}-{:02} {}:{:02}:{:02}",
-            dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second,
+    let led = Output::new(p.PIN_13, Level::Low);
+    cortex_m::interrupt::free(|cs| {
+        rtc.schedule_alarm(
+            DateTimeFilter::default()
+                .second(5)
         );
-    }
+        LED.borrow(cs).borrow_mut().replace(led);
+        RTC.borrow(cs).borrow_mut().replace(rtc);
+        unsafe {
+            cortex_m::peripheral::NVIC::unmask(interrupt::RTC_IRQ);
+        }
+    });
 
     loop {
-        log::info!("iterate");
-        Timer::after_secs(2).await;
+        wfi(); // TODO vs wfe();
     }
+}
+
+#[interrupt]
+fn RTC_IRQ() {
+    cortex_m::interrupt::free(|cs| {
+        let mut rtc = RTC.borrow(cs).borrow_mut();
+        let rtc = rtc.as_mut().unwrap();
+        let mut led = LED.borrow(cs).borrow_mut();
+        let led = led.as_mut().unwrap();
+
+        led.set_high();
+
+        rtc.clear_interrupt();
+    });
 }
